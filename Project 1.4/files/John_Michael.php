5f43d0e1be6a99cc9bba41489f410319d090381c
villacastinjero06-rgb <villacastinjero06@gmail.com>
2025-12-18 02:45:54 +0800

    Fur Das 1.4

---
+++ Project 1.4/files/John_Michael.php
@@ -0,0 +1,19 @@
+<?php
+$requiredFile = "John_Michael.txt";
+                "John_Michael.txt";
+                "JM_docblock.php";
+                "JM_GRADE.php";
+                "JM_Login.php";
+                "JM_multi_line.php";
+                "JM_single_line.php";
+                "JM_triangles.php";
+                "JM_Variable.php";
+                "JM_VOTE.php";
+include 'guard.php';
+include __DIR__ . '/../users.php';
+$me = $_SESSION['adminuser'] ?? null;
+if ($users == null || !$me || !isset($users[$me]) || !in_array($requiredFile, $users[$me]['files'])) {
+    http_response_code(403);
+    exit('Access denied');
+}
+?>
